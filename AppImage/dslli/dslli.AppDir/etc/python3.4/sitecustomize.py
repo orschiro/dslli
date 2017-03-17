39c688ff4686623dcc66d3a5a152ae076107242d
Robert Orzanna <robert@orzanna.de>
2017-03-17 22:18:46 +0100

    Removed AppImage

--- AppImage/dslli/dslli.AppDir/etc/python3.4/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys,os
-if sys.version_info[0] < 3:
-    prefix = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.path[0]))))
-    sys.path = [ prefix+s for s in sys.path if not s.startswith(prefix) ]
